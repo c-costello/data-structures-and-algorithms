9085c3972a1a2e5357a2c75469b9929e7ba79d39
Clarice Costello <claricostello@outlook.com>
2019-01-10 09:54:20 -0800

    wrote failing test to return index 0

--- Challenges/array_binary_search/BinarySearchApp/BinarySearchTest/UnitTest1.cs
+++ Challenges/array_binary_search/BinarySearchApp/BinarySearchTest/UnitTest1.cs
@@ -21,5 +21,30 @@ namespace BinarySearchTest
             int num = 3;
             Assert.Equal(2, Program.BinarySearch(arr, num));
         }
+        [Fact]
+        public void CanReturnIndexOne()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int num = 2;
+            Assert.Equal(1, Program.BinarySearch(arr, num));
+
+        }
+        [Fact]
+        public void CanReturnIndexFour()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int num = 5;
+            Assert.Equal(4, Program.BinarySearch(arr, num));
+
+        }
+        [Fact]
+        public void CanReturnIndexZero()
+        { 
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int num = 1;
+            Assert.Equal(0, Program.BinarySearch(arr, num));
+
+        }
+    
     }
 }
