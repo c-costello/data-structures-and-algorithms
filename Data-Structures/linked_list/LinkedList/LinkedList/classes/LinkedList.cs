943752f95acf1011509c7fd77853f03952823b0f
Clarice Costello <claricostello@outlook.com>
2019-01-14 09:17:49 -0800

    completed Insert method

--- Data-Structures/linked_list/LinkedList/LinkedList/classes/LinkedList.cs
+++ Data-Structures/linked_list/LinkedList/LinkedList/classes/LinkedList.cs
@@ -9,5 +9,14 @@ namespace LinkedList.classes
         public Node Head { get; set; }
         public Node Current { get; set; }
 
+        // insert
+        public void Insert(int value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
     }
+
 }
