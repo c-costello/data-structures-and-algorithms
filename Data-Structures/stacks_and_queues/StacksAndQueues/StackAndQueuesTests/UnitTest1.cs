c0920c42438b2016e61fafe860516939b247219f
Clarice Costello <claricostello@outlook.com>
2019-01-22 09:22:56 -0800

    CanCreateEmptyStack test

---
+++ Data-Structures/stacks_and_queues/StacksAndQueues/StackAndQueuesTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using StacksAndQueues;
+
+namespace StackAndQueuesTests
+{
+    public class UnitTest1
+    {
+
+        //Stack tests
+
+        //Constructor test
+        [Fact]
+        public void CanCreateEmptyStack()
+        {
+            Stack stack = new Stack();
+
+            Assert.Null(stack.Top);
+        }
+    }
+}
