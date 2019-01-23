cf8ed5d3bb1089960c00ab5f2e1b34a8435a1377
Clarice Costello <claricostello@outlook.com>
2019-01-22 21:02:07 -0800

    wrote enqueue tests

--- Challenges/queue_with_stacks/queue_with_stacks/PsuedoQueueTests/UnitTest1.cs
+++ Challenges/queue_with_stacks/queue_with_stacks/PsuedoQueueTests/UnitTest1.cs
@@ -8,5 +8,36 @@ namespace PsuedoQueueTests
     public class UnitTest1
     {
 
+        // enqueue tests
+
+        [Fact]
+        public void CanEnqueueIntoAnEmptyQueue()
+        {
+            PsuedoQueue pQ = new PsuedoQueue();
+            pQ.Enqueue(5);
+
+            Assert.Equal(5, pQ.Top.Value);
+
+        }
+        [Fact]
+        public void LastInNodeIsLastInQueue()
+        {
+            PsuedoQueue pQ = new PsuedoQueue();
+            pQ.Enqueue(5);
+            pQ.Enqueue(6);
+            pQ.Enqueue(7);
+
+            Assert.Equal(7, pQ.Top.Next.Value);
+        }
+
+        [Fact]
+        public void FirstInNodeIsFirstInQueue()
+        {
+            PsuedoQueue pQ = new PsuedoQueue();
+            pQ.Enqueue(5);
+            pQ.Enqueue(7);
+            pQ.Enqueue(9);
+            Assert.Equal(5, pQ.Top.Value);
+        }
     }
 }
