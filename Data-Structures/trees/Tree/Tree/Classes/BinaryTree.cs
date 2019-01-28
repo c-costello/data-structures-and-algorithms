cb3de11b243826522e95b32f16359baae6bab854
Clarice Costello <claricostello@outlook.com>
2019-01-28 10:34:45 -0800

    created binarytree class with three methods of traversal

--- Data-Structures/trees/Tree/Tree/Classes/BinaryTree.cs
+++ Data-Structures/trees/Tree/Tree/Classes/BinaryTree.cs
@@ -1,10 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using StacksAndQueues;
 
 namespace Tree.Classes
 {
     class BinaryTree
     {
+
+        public Node Root { get; set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+
+        }
+
+        public BinaryTree(Node node)
+        {
+            Root = node;
+        }
+    
+
+        public int[] PreOrder(Node root)
+        {   
+            List<int> list = new List<int>();
+            list.Add(root.Value);            
+            if (root.LeftChild != null)
+            {
+                PreOrder(root.LeftChild, list);
+            }
+            if (root.RightChild != null)
+            {
+                PreOrder(root.RightChild, list);
+            }
+            int[] arr = list.ToArray();
+            //Console.WriteLine(String.Join(" ",arr));
+            return arr;
+
+        }
+
+        public int[] PreOrder(Node root, List<int> list)
+        {
+            list.Add(root.Value);
+            if (root.LeftChild != null)
+            {
+                PreOrder(root.LeftChild, list);
+            }
+            if (root.RightChild != null)
+            {
+                PreOrder(root.RightChild, list);
+            }
+            int[] arr = list.ToArray();
+            //Console.WriteLine(String.Join(" ",arr));
+            return arr;
+
+        }
+
+        public int[] InOrder(Node root)
+        {
+            List<int> list = new List<int>();
+            if (root.LeftChild != null)
+            {
+                InOrder(root.LeftChild, list);
+            }
+            list.Add(root.Value);
+            if (root.RightChild != null)
+            {
+                InOrder(root.RightChild, list);
+            }
+            int[] arr = list.ToArray();
+            return arr;
+
+        }
+
+        public int[] InOrder(Node root, List<int> list)
+        {
+            if (root.LeftChild != null)
+            {
+                InOrder(root.LeftChild, list);
+            }
+            list.Add(root.Value);
+            if (root.RightChild != null)
+            {
+                InOrder(root.RightChild, list);
+            }
+            int[] arr = list.ToArray();
+            return arr;
+
+        }
+
+
+
+        public int[] PostOrder(Node root)
+        {
+            List<int> list = new List<int>();
+            if (root.LeftChild != null)
+            {
+                PostOrder(root.LeftChild, list);
+            }
+            if (root.RightChild != null)
+            {
+                PostOrder(root.RightChild, list);
+            }
+            list.Add(root.Value);
+            int[] arr = list.ToArray();
+            return arr;
+
+        }
+
+        public int[] PostOrder(Node root, List<int> list)
+        {
+            if (root.LeftChild != null)
+            {
+                PostOrder(root.LeftChild, list);
+            }
+            if (root.RightChild != null)
+            {
+                PostOrder(root.RightChild, list);
+            }
+            list.Add(root.Value);
+            int[] arr = list.ToArray();
+            return arr;
+
+        }
+        public void InOrder2(Node root)
+        {
+            if (root.LeftChild != null)
+            {
+                InOrder2(root.LeftChild);
+            }
+            Console.Write(" " + root.Value);
+            if (root.RightChild != null)
+            {
+                InOrder2(root.RightChild);
+            }
+        }
+
+        public void PostOrder2(Node root)
+        {
+            if (root.LeftChild != null)
+            {
+                PostOrder2(root.LeftChild);
+            }
+            if (root.RightChild != null)
+            {
+                PostOrder2(root.RightChild);
+            }
+            Console.Write(" " + root.Value);
+
+        }
     }
 }
--- Data-Structures/trees/Tree/Tree/Program.cs
+++ Data-Structures/trees/Tree/Tree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tree.Classes;
 
 namespace Tree
 {
@@ -7,6 +8,27 @@ namespace Tree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BinaryTree bTree = new BinaryTree();
+
+            bTree.Root = new Node(50);
+            bTree.Root.LeftChild = new Node(300);
+            bTree.Root.RightChild = new Node(22);
+            bTree.Root.LeftChild.LeftChild = new Node(75);
+            bTree.Root.LeftChild.RightChild = new Node(11);
+            bTree.Root.RightChild.LeftChild = new Node(3);
+            bTree.Root.RightChild.RightChild = new Node(100);
+            Console.WriteLine("=====================PreOrder===================");
+            Console.Write(String.Join(" ", bTree.PreOrder(bTree.Root)));
+            Console.WriteLine();
+            Console.WriteLine("=====================InOrder===================");
+            Console.Write(String.Join(" ", bTree.InOrder(bTree.Root)));
+            Console.WriteLine();
+            Console.WriteLine("=========================");
+            bTree.InOrder2(bTree.Root);
+            Console.WriteLine();
+            Console.WriteLine("+++++++++PostOrder+++++++++++++++++");
+            Console.Write(String.Join(" ", bTree.PostOrder(bTree.Root)));
+            Console.WriteLine();
         }
     }
 }
