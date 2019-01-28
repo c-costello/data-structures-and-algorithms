9c6df1d25f559d80bb11306692f7732a272f61f1
Clarice Costello <claricostello@outlook.com>
2019-01-28 09:22:45 -0800

    added node class

---
+++ Data-Structures/trees/Tree/Tree/Classes/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class Node
+    {
+        public int Value { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+        public Node(int value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+            
+        }
+    }
+}
