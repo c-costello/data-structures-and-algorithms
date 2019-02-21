cab4bb71ee6e19ca2138afe2fc9130b08e09ea51
Clarice Costello <claricostello@outlook.com>
2019-02-21 13:43:17 -0800

    resubmit changes

--- Challenges/repeated_word/RepeatedWordApp/RepeatedWordApp/Program.cs
+++ Challenges/repeated_word/RepeatedWordApp/RepeatedWordApp/Program.cs
@@ -8,8 +8,7 @@ namespace RepeatedWordApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(RepeatedWord("cat, cat "));
-            Console.WriteLine(RepeatedWord("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."));
+            //Console.WriteLine(RepeatedWord("cat, cat "));
             Console.WriteLine(RepeatedWord("It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."));
             Console.ReadLine();
         }
@@ -27,7 +26,7 @@ namespace RepeatedWordApp
             for (int i = 0; i < arr.Length; i++)
             {
 
-                if (hashtable.Contains(arr[i].ToLower()))
+                if (hashtable.Contains(arr[i].ToLower()) && arr[i] != "") 
                 {
                     return arr[i].ToLower();
                 }
