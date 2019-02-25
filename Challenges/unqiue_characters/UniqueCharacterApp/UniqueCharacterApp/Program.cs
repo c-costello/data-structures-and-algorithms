5fa8590d0ae30f81f50f75fe5a041845399f55e6
Clarice Costello <claricostello@outlook.com>
2019-02-25 11:09:31 -0800

    wrote out NoDuplicates

---
+++ Challenges/unqiue_characters/UniqueCharacterApp/UniqueCharacterApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using hashtablesApp.Classes;
+using System;
+
+namespace UniqueCharacterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NoDuplicates("Hello"));
+            Console.WriteLine(NoDuplicates("the sad cow"));
+
+        }
+
+        public static bool NoDuplicates(string str)
+        {
+            Hashtable hashtable = new Hashtable();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i] != ' ')
+                {
+                    if (hashtable.Contains(str[i].ToString()))
+                    {
+                        return false;
+                    }
+                    hashtable.Add(str[i].ToString(), str[i].ToString());
+                }
+            }
+            return true;
+        }
+    }
+}
